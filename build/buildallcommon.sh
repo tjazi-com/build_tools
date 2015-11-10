c3d974b882e640fbf1d8d806507c77cef08a7612
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2015-11-10 09:02:42 +0000

    Adding new components to buildallcommon.sh

--- build/buildallcommon.sh
+++ build/buildallcommon.sh
@@ -4,8 +4,10 @@ set -e
 
 projectstobuild=("lib_messaging_rest"
 				 "profiles/messages" "profiles/client"
+				 "profiles_authorizer/messages" "profiles_authorizer/client"
+				 "profiles_authenticator/messages" "profiles_authenticator/client"
 				 "profiles_creator/messages" "profiles_creator/client"
-				 "security/messages" "security/client"
+				 "security/messages" "security/client" "security/common"
 				 )
 
 for project in ${projectstobuild[@]}
